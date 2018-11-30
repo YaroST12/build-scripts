6449f942b22ec19014e1d1275ff34cb7b109d97b
Yaroslav Furman <yaro330@gmail.com>
2018-11-30 18:52:20 +0300

    build.sh: cumulative update
    - Add build clean
    - Changed TC locations
    - Improved Clang versioning
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ export KBUILD_BUILD_USER="ST12"
 export ARCH="arm64"
 export CLANG_TRIPLE="aarch64-linux-gnu-"
 # Fucking toolchains
-CC="${TTHD}/toolchains/gcc-linaro-7.3.1-aarch64/bin/aarch64-linux-gnu-"
-CC_32="${TTHD}/toolchains/gcc-linaro-7.3.1-arm/bin/arm-linux-gnueabi-"
+GCC="${TTHD}/toolchains/aarch64-linux-gnu/bin/aarch64-linux-gnu-"
+GCC_32="${TTHD}/toolchains/gcc-linaro-7.3.1-arm/bin/arm-linux-gnueabi-"
 CT="${TTHD}/toolchains/clang-8.x/bin/clang"
 
 # Colors
@@ -40,6 +40,7 @@ function parse_parameters() {
 	PARAMS="${*}"
 	# Default params
 	BUILD_GCC=false
+	BUILD_CLEAN=false
 	CONFIG_FILE="z2_row_defconfig"
 	DEVICE="row"
 	TC="${YEL}Flash-Clang${LGR}"
@@ -56,6 +57,9 @@ function parse_parameters() {
 				objdir="${kernel_dir}/out_gcc"
 				TC="GCC"
 				BUILD_GCC=true ;;
+
+			"-c"|"--clean")
+				BUILD_CLEAN=true ;;
 
             *) die "Invalid parameter specified!" ;;
 		esac
@@ -83,7 +87,13 @@ function format_time() {
 function make_image()
 {
 	# Needed to make sure we get dtb built and added to kernel image properly
-	rm -rf ${objdir}/arch/arm64/boot/
+	# Cleanup existing build files
+	if [ ${BUILD_CLEAN} == true ]; then
+	    rm -rf ${objdir}
+		echo -e ${LGR} "Cleaning up mess ${NC}"
+	else
+	    rm -rf ${objdir}/arch/arm64/boot/
+	fi
 	START=$(date +%s)
 	echo -e ${LGR} "Generating Defconfig ${NC}"
 	make -s ARCH=${ARCH} O=${objdir} ${CONFIG_FILE}
@@ -91,7 +101,7 @@ function make_image()
 	echo -e ${LGR} "Building image ${NC}"
 	if [ ${BUILD_GCC} == true ]; then
 		cd ${kernel_dir}
-		make -s -j${cpus} CROSS_COMPILE=${CC} CROSS_COMPILE_ARM32=${CC_32} \
+		make -s -j${cpus} CROSS_COMPILE=${GCC} CROSS_COMPILE_ARM32=${GCC_32} \
 		O=${objdir} Image.gz-dtb
 	else
 		POLLY="-mllvm -polly \
@@ -103,11 +113,11 @@ function make_image()
 			-mllvm -polly-vectorizer=stripmine"
 
 		export KBUILD_COMPILER_STRING="clang-$($CT --version | \
-		grep "clang version" | cut -c 15-24 | sed -e 's/ //')"
+		grep "clang version" | cut -c 15- | sed -e 's/ (.*//g')"
 
 		cd ${kernel_dir}
-		make -s -j${cpus} CC="${CT} ${POLLY}" CROSS_COMPILE=${CC} \
-		CROSS_COMPILE_ARM32=${CC_32} O=${objdir} Image.gz-dtb
+		make -s -j${cpus} CC="${CT} ${POLLY}" CROSS_COMPILE=${GCC} \
+		CROSS_COMPILE_ARM32=${GCC_32} O=${objdir} Image.gz-dtb
 	fi
 	END=$(date +%s)
 }
@@ -116,11 +126,7 @@ function completion()
 	cd ${objdir}
 	COMPILED_IMAGE=arch/arm64/boot/Image.gz-dtb
 	if [[ -f ${COMPILED_IMAGE} ]]; then
-		if [ ${DEVICE} == "plus" ]; then
-			mv -f ${COMPILED_IMAGE} ${builddir}/Image.gz-dtb_plus
-		else
-			mv -f ${COMPILED_IMAGE} ${builddir}/Image.gz-dtb_row
-		fi
+		mv -f ${COMPILED_IMAGE} ${builddir}/Image.gz-dtb_${DEVICE}
 		echo -e ${LGR} "Build for Z2_$DEVICE competed in" \
 			"$(format_time "${START}" "${END}")!${NC}"
 		echo -e ${LGR} ${SEP}
